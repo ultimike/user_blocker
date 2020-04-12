3ca2ed993f76ac6ff03f9c48ece5808191f7b1b8
Michael Anello <manello@gmail.com>
2020-04-12 13:01:07 -0400

    Refactored based on Drush scaffolding.

--- src/Form/BlockerForm.php
+++ src/Form/BlockerForm.php
@@ -6,33 +6,34 @@ use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 
 /**
- * Class BlockerForm.
+ * Provides a User blocker form.
  */
 class BlockerForm extends FormBase {
-
 
   /**
    * {@inheritdoc}
    */
   public function getFormId() {
-    return 'user_blocker_form';
+    return 'user_blocker_blocker';
   }
 
   /**
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
-    $form['username'] = [
-      '#type' => 'textfield',
-      '#title' => $this->t('Username'),
-      '#description' => $this->t('Enter the username of the user you want to block.'),
-      '#maxlength' => 64,
-      '#size' => 64,
-      '#weight' => '0',
+
+    $form['message'] = [
+      '#type' => 'textarea',
+      '#title' => $this->t('Message'),
+      '#required' => TRUE,
     ];
-    $form['submit'] = [
+
+    $form['actions'] = [
+      '#type' => 'actions',
+    ];
+    $form['actions']['submit'] = [
       '#type' => 'submit',
-      '#value' => $this->t('Submit'),
+      '#value' => $this->t('Send'),
     ];
 
     return $form;
@@ -42,18 +43,17 @@ class BlockerForm extends FormBase {
    * {@inheritdoc}
    */
   public function validateForm(array &$form, FormStateInterface $form_state) {
-    parent::validateForm($form, $form_state);
+    if (mb_strlen($form_state->getValue('message')) < 10) {
+      $form_state->setErrorByName('name', $this->t('Message should be at least 10 characters.'));
+    }
   }
 
   /**
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    // Display result.
-    foreach ($form_state->getValues() as $key => $value) {
-      drupal_set_message($key . ': ' . $value);
-    }
-
+    $this->messenger()->addStatus($this->t('The message has been sent.'));
+    $form_state->setRedirect('<front>');
   }
 
 }
